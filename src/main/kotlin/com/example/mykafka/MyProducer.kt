d250d7bab5d2539ebb0e93653233be91a7cce0de
tfutada <tafutada@gmail.com>
2024-04-19 17:43:27 +0900

    producer

---
+++ src/main/kotlin/com/example/mykafka/MyProducer.kt
@@ -0,0 +1,15 @@
+package com.example.mykafka
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.stereotype.Component;
+
+@Component
+class MyProducer {
+    @Autowired
+    lateinit var kafkaTemplate: KafkaTemplate<String, String>
+
+    fun sendMessage(message: String) {
+        kafkaTemplate.send("simple-message-topic", message)
+    }
+}
